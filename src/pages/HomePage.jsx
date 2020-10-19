447bd7500bb33b155591e27fb1fd3935fdc52297
Lelianto Eko Pradana <lelianto.eko@yesdok.com>
2020-10-19 20:51:52 +0700

    update font style and link to messenger

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -44,8 +44,7 @@ const useStyles = makeStyles({
         marginTop: '30px'
     },
     backgroundSubText: {
-        background: '#E8FFFF',
-        padding: '50px 0px'
+        padding: '10px 50px'
     },
     imageWidth: {
         width: '50%'
@@ -58,6 +57,40 @@ const useStyles = makeStyles({
         fontSize: '16px',
         lineHeight: '2rem'
     },
+    handleSurvey: {
+        fontSize: '48px',
+        fontWeight: '600',
+        marginBottom: '10px'
+    },
+    handleBox: {
+        background: 'yellow',
+        padding: '10px',
+        borderRadius: '25px'
+    },
+    quoteFont: {
+        fontSize: '48px',
+        fontStyle: 'italic',
+        fontWeight: '500'
+    },
+    quoteFontM: {
+        fontSize: '24px',
+        fontStyle: 'italic',
+        fontWeight: '500'
+    },
+    buttonPrimary: {
+        backgroundColor: 'yellow !important',
+        color: 'black !important',
+        fontWeight: '700',
+        fontSize: '48px',
+        padding: '0px 30px',
+        borderRadius: '15px'
+    },
+    handleLink: {
+        textDecoration: 'none',
+        '&:hover': {
+            textDecoration: 'none'
+        }
+    }
 })
 
 const HomePage = (props) => {
@@ -68,6 +101,7 @@ const HomePage = (props) => {
     const classDouble1 = [classes.backgroundSubText, 'col-12'].join(' ')
     const subText1 = [classes.handleMarginTop, 'row'].join(' ')
     const subTextM = [classes.handleMarginTop1, 'row'].join(' ')
+    let link = 'https://m.me/101758411513631'
     return (
         <React.Fragment>
             <div className='container-fluid'>
@@ -97,17 +131,25 @@ const HomePage = (props) => {
                                     <div className='col-9'>
                                         <div className={classes.textControl}>
                                             <strong>
-                                                Berat, menjadi kata yang tepat untuk tahun ini. Masalah pandemi yang berujung
+                                                Berat,&nbsp;
+                                    </strong>
+                                                menjadi kata yang tepat untuk tahun ini. Masalah pandemi yang berujung
                                                 pada kesulitan ekonomi dan berubahnya “normal” yang biasa kita jalani
                                                 memunculkan penyakit baru, yaitu penyakit hati.
-                                    </strong>
                                         </div>
                                         <div className={[classes.textControl, classes.handleSubText].join(' ')}>
-                                            <strong>
-                                                Dilansir dari republika.co.id, studi terbaru oleh University College London (UCL),
-                                                Imperial College dan University of Sussex mencatat, bahwa dari 10 anak muda,
-                                                6 diantaranya memiliki masalah kesehatan mental.
-                                    </strong>
+                                            Dilansir dari republika.co.id, studi terbaru oleh University College London (UCL),
+                                            Imperial College dan University of Sussex mencatat, bahwa
+                                            <div className="text-center">
+                                                <div className={classes.handleSurvey}>
+                                                    6 dari 10
+                                                </div>
+                                                anak muda memiliki <br />
+                                                masalah&nbsp;
+                                                <strong>
+                                                    kesehatan mental.
+                                                </strong>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -116,10 +158,11 @@ const HomePage = (props) => {
                                     <div className='col-12'>
                                         <div className={classes.textControlSm}>
                                             <strong>
-                                                Berat, menjadi kata yang tepat untuk tahun ini. Masalah pandemi yang berujung
+                                                Berat,&nbsp;
+                                            </strong>
+                                                menjadi kata yang tepat untuk tahun ini. Masalah pandemi yang berujung
                                                 pada kesulitan ekonomi dan berubahnya “normal” yang biasa kita jalani
                                                 memunculkan penyakit baru, yaitu penyakit hati.
-                                            </strong>
                                         </div>
                                     </div>
 
@@ -131,11 +174,18 @@ const HomePage = (props) => {
 
                                     <div className='col-12'>
                                         <div className={[classes.textControlSm, classes.handleSubText].join(' ')}>
-                                            <strong>
-                                                Dilansir dari republika.co.id, studi terbaru oleh University College London (UCL),
-                                                Imperial College dan University of Sussex mencatat, bahwa dari 10 anak muda,
-                                                6 diantaranya memiliki masalah kesehatan mental.
-                                            </strong>
+                                            Dilansir dari republika.co.id, studi terbaru oleh University College London (UCL),
+                                            Imperial College dan University of Sussex mencatat, bahwa
+                                            <div className="text-center">
+                                                <div className={classes.handleSurvey}>
+                                                    6 dari 10
+                                                </div>
+                                                anak muda memiliki <br />
+                                                masalah&nbsp;
+                                                <strong>
+                                                    kesehatan mental.
+                                                </strong>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -144,9 +194,11 @@ const HomePage = (props) => {
                 </div>
                 <div className='row mt-5'>
                     <div className={classDouble1}>
-                        <strong className={matches ? classes.textControl : classes.textControlSm}>
-                            Dalam keadaan ini, kita tahu bahwa harapan sesungguhnya hanya bisa kita temukan di dalam Tuhan saja.
+                        <div className={classes.handleBox}>
+                            <strong className={matches ? classes.textControl : classes.textControlSm}>
+                                Dalam keadaan ini, kita tahu bahwa harapan sesungguhnya hanya bisa kita temukan di dalam Tuhan saja.
                         </strong>
+                        </div>
                     </div>
                 </div>
                 <div className='row mt-5'>
@@ -156,15 +208,23 @@ const HomePage = (props) => {
                 </div>
                 <div className={matches ? subText1 : subTextM}>
                     <div className='col-12'>
-                        <strong className={matches ? classes.textControl : classes.textControlSm}>
-                            Kalau kamu ingin menemukan harapan di dalam Tuhan, kita dapat membicarakannya bersama.
-                            Kami bersedia menjadi teman perjalanan yang akan menemanimu untuk menemukan
-                            harapan yang sejati di dalam Tuhan itu. Cerita dan informasi yang kamu bagikan akan dijaga keamanan dan
-                            kerahasiaannya tanpa dibagikan kepada pihak lain.
-                            <br />
-                            <br />
-                            “ Let your mess become message, your test become testimony”
-                        </strong>
+                        <div className={matches ? classes.textControl : classes.textControlSm}>
+                            <div className='text-center'>
+                                Kalau kamu ingin menemukan harapan di dalam Tuhan, kita dapat membicarakannya bersama.
+                                Kami bersedia menjadi&nbsp;
+                                <strong>
+                                    teman perjalanan&nbsp;
+                                </strong>
+                                yang akan menemanimu untuk menemukan
+                                harapan yang sejati di dalam Tuhan itu. Cerita dan informasi yang kamu bagikan akan dijaga keamanan dan
+                                kerahasiaannya tanpa dibagikan kepada pihak lain.
+                                <br />
+                                <br />
+                                <strong className={matches ? classes.quoteFont : classes.quoteFontM}>
+                                    “Let your mess become message, your test become testimony”
+                                </strong>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className={subText1}>
@@ -177,9 +237,11 @@ const HomePage = (props) => {
                         </strong>
                     </div>
                     <div className='col-12 mt-3 mb-5'>
-                        <Button variant="contained" color="primary" disableElevation>
-                            Kirim Pesan
-                        </Button>
+                        <a href={link} className={classes.handleLink} rel="noopener noreferrer" target="_blank">
+                            <Button variant="contained" color="primary" className={classes.buttonPrimary} disableElevation>
+                                Kirim Pesan
+                            </Button>
+                        </a>
                     </div>
                 </div>
             </div>
